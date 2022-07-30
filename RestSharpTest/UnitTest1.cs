50861cdbc00bfc5a51cc44e713a6ec3c6a603851
000VIJAY <vijay893689@gmail.com>
2022-07-30 16:33:11 +0530

    Vijay Added Program test for add employee in json server

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
+using System.Net;
 
 namespace RestSharpTest
 {
@@ -35,6 +37,21 @@ namespace RestSharpTest
             {
                 Console.WriteLine("id: " + employee.Id + " Name: " + employee.Name + " Salary: " + employee.Salary);
             }
+        }
+        [TestMethod]
+        public void givenEmployee_ShouldReturnEmployee()
+        {
+            RestRequest request = new RestRequest("/Employee",Method.Post);
+            JObject jObjectBody = new JObject();
+            jObjectBody.Add("name"," Umesh Kumar");
+            jObjectBody.Add("Salary"," 60000");
+            request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            Employee responseEmployee = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Umesh Kumar", responseEmployee.Name);
+            Assert.AreEqual("60000",responseEmployee.Salary);
         }
     }
 }
