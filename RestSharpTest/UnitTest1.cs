4d0e4568f28e73166e9fd91458b274f74d287183
000VIJAY <vijay893689@gmail.com>
2022-07-30 17:14:54 +0530

    Vijay program for update json server with rest sharp

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -30,9 +30,9 @@ namespace RestSharpTest
         public void callingGetApi_ReturnEmployeeList()
         {
             RestResponse response = getEmployeeList();
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<Employee> employees = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(employees.Count, 11);
+            Assert.AreEqual(employees.Count, 10);
             foreach (Employee employee in employees)
             {
                 Console.WriteLine("id: " + employee.Id + " Name: " + employee.Name + " Salary: " + employee.Salary);
@@ -41,10 +41,11 @@ namespace RestSharpTest
         [TestMethod]
         public void givenEmployee_ShouldReturnEmployee()
         {
+            Employee employee = new Employee(); 
             RestRequest request = new RestRequest("/Employee",Method.Post);
             JObject jObjectBody = new JObject();
-            jObjectBody.Add("name"," Umesh Kumar");
-            jObjectBody.Add("Salary"," 60000");
+            jObjectBody.Add("name","Umesh Kumar");
+            //jObjectBody.Add("Salary"," 60000");
             request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
             RestResponse response = client.Execute(request);
 
@@ -52,6 +53,22 @@ namespace RestSharpTest
             Employee responseEmployee = JsonConvert.DeserializeObject<Employee>(response.Content);
             Assert.AreEqual("Umesh Kumar", responseEmployee.Name);
             Assert.AreEqual("60000",responseEmployee.Salary);
+        }
+        [TestMethod]
+        public void givenEmployee_UpdateEmployee_ShouldReturnEmployee()
+        {
+            RestRequest request = new RestRequest("http://localhost:3000/Employee/17", Method.Put);
+            JObject jObjectBody = new JObject();
+            jObjectBody.Add("name", "Rohan");
+            jObjectBody.Add("Salary", "55000");
+            request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Employee responseEmployee = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Rohan", responseEmployee.Name);
+            Assert.AreEqual("55000", responseEmployee.Salary);
+            Console.WriteLine(response.Content);
         }
     }
 }
